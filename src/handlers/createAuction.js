f6bd9fee3cfca24e6bd109ccfca9289e5028aada
Patrick <patrick.ortell@arus.io>
2021-03-02 07:52:57 -0700

    get this off my box

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,41 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+import commonMiddleware from '../lib/commonMiddleware';
+import createError from "http-errors";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createAuction(event, context) {
+  const { title } = event.body;
+  const now = new Date();
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: "OPEN",
+    createdAt: now.toISOString(),
+    highestBid: {
+      amount: 0,
+
+    },
+  };
+
+  try {
+    await dynamodb
+      .put({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Item: auction,
+      })
+      .promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = commonMiddleware(createAuction);
